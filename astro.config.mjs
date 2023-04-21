b3177470ef4b7fc4e11344b67aaf7a69e2925d3f
Luuk <luukbrauckmann@gmail.com>
2023-04-21 11:21:27 +0200

    Vercel adapter+

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server'
+	output: 'server',
+	adapter: vercel()
 });
